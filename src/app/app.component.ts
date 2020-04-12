f22532fabb1cfb6c7e3a6c5bbddac8ad8671f86e
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-12 17:55:30 +0200

    Set and Patch Form Values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,4 +20,27 @@ export class AppComponent {
   onSubmit(): void {
     console.log(this.signUpForm);
   }
+
+  suggestUsername(): void {
+    const suggestedName = 'Superuser';
+
+    // First method but not ideal as we need to specify all the controls
+    // and we may override already entered values
+    // this.signUpForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: this.defaultQuestion,
+    //   secretAnswer: '',
+    //   gender: this.defaultGender
+    // });
+
+    // This method is the best as we can specify only the value of the control targeted
+    this.signUpForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+  }
 }
