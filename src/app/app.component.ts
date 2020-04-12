20fe287f3a5a0a06c18be845c7d72e5ea7272515
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-12 18:11:48 +0200

    Displaying submitting data as a summary after the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,30 @@ export class AppComponent {
   readonly defaultGender = 'male';
   answer = '';
   genders = ['male', 'female', 'other'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   // onSubmit(form: NgForm): void {
   //   console.log(form);
   // }
 
+  // onSubmit(): void {
+  //   console.log(this.signUpForm);
+  // }
+
   onSubmit(): void {
-    console.log(this.signUpForm);
+    this.submitted = true;
+    this.user.username = this.signUpForm.value.userData.username;
+    this.user.email = this.signUpForm.value.userData.email;
+    this.user.secretQuestion = this.signUpForm.value.secret;
+    this.user.answer = this.signUpForm.value.secretAnswer;
+    this.user.gender = this.signUpForm.value.gender;
   }
 
   suggestUsername(): void {
